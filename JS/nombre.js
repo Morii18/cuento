1ccd146b9719f2de9e1202c1e460e9e4177cf1e0
Mora Marranghello <marranghellomora@gmail.com>
2026-03-31 11:09:52 -0300

    todo listo falta ortografia

--- JS/nombre.js
+++ JS/nombre.js
@@ -1,3 +1,4 @@
+function CUENTO(){
 let nombre = prompt("¿Cómo te llamás?");
 let camino = prompt("A " + nombre +" la llaman: Aceptar o Rechazar ");
 
@@ -7,20 +8,22 @@ if (camino === "Aceptar") {
             let final = prompt(nombre + " ya se encuentra en la cabaña, pero hay olor a cadaver:¿Entrar o salir corriendo");
             if (final === "Entrar"){
                 alert("Detras de la puerta se encontraba un asesino, te mata. " + nombre + " has perdido")
-            } else{
+            } else if(final === "salir corriendo"){
                 let correr = prompt("Sales corriendo a un lugar que parece seguro, intentas pedri ayuda? si o no?");
-                if (correr === "si"){
+            if(correr === "si"){
                     alert(nombre + " no hay señal, los ruidos de la cabaña eran un asesino te encuentra y muertes, FINAL.");
-                } else (correr === "no") 
+                } else if (correr === "no") {
                     alert(nombre + " Mueres" );
                 }
-            } else(accion === "no ir")
+                } 
+            } else if(accion === "no ir"){
                 alert(nombre + " respondes amablemnte que no puedes ir, te salvaste!!");
-        } 
+        }
+    }
 else if(camino == "Rechazar"){
     alert(nombre + " te has salvado");
 }
-
+}
 
 
 
